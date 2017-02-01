5d0f67fb6a082533863b5b4457124ede7d938dcc
hemsundar9 <hemsundar9>
2017-02-01 15:39:05 +0530

    indentation

--- src/main/resources/db/migration/V4__create_city_schema.sql
+++ src/main/resources/db/migration/V4__create_city_schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `cities` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `version` INT NOT NULL DEFAULT 0,
-  `name` VARCHAR(45) NOT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  `day_rate` DOUBLE NOT NULL DEFAULT 0.0,
-  `night_rate` DOUBLE NOT NULL DEFAULT 0.0,
-  `created` TIMESTAMP NOT NULL DEFAULT now(),
-  `modified` TIMESTAMP NULL DEFAULT now(),
+  `id`            INT         NOT NULL    AUTO_INCREMENT,
+  `version`       INT         NOT NULL    DEFAULT 0,
+  `name`          VARCHAR(45) NOT NULL,
+  `state`         VARCHAR(45) NOT NULL,
+  `day_rate`      DOUBLE      NOT NULL    DEFAULT 0.0,
+  `night_rate`    DOUBLE      NOT NULL    DEFAULT 0.0,
+  `created`       TIMESTAMP   NOT NULL    DEFAULT now(),
+  `modified`      TIMESTAMP   NULL        DEFAULT now(),
   PRIMARY KEY (`id`));
